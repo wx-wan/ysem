adac535229ec0cba92e4f82af3d484dc7f36741f
万文星 <wanwx@olymtech.com>
2026-08-22 16:40:57 +0800

    UI: 产品卡片视图「单位」标签改为「模式」

--- client/src/components/product/cards/ProductCard.tsx
+++ client/src/components/product/cards/ProductCard.tsx
@@ -105,7 +105,7 @@ const ProductCard = memo(function ProductCard({
             </span>
           </div>
           <div className="pm-prod-meta__item">
-            <span className="pm-prod-meta__label">单位</span>
+            <span className="pm-prod-meta__label">模式</span>
             <span className="pm-prod-meta__value">
               {product.unit === '套' ? '组合' : '单品'}
             </span>
