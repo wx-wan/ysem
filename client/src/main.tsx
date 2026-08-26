a6bd06bc8dc27773f4ffb2ef97c793d2f6b07981
bibo <wanwx@olymtech.com>
2026-08-26 21:46:26 +0800

    chore: 加回 v7_startTransition future flag，消除 React Router 警告

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -40,7 +40,7 @@ function Root() {
       }}
     >
       <AntdApp>
-        <BrowserRouter future={{ v7_relativeSplatPath: true }}>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <App />
         </BrowserRouter>
       </AntdApp>
